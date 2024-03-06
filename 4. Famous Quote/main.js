5b5bb39e42c1428868e970f211c5ab19c217074e
Kanchan Heera <161306339+kanchanheera@users.noreply.github.com>
2024-03-06 18:02:21 +0500

    Add files via upload

---
+++ 4. Famous Quote/main.js
@@ -0,0 +1,2 @@
+var author = "Albert Einstein";
+console.log("".concat(author, " once said, \"A person who never made a mistake never tried anything new.\""));
---
+++ 4. Famous Quote/main.ts
@@ -0,0 +1,2 @@
+let author = "Albert Einstein";
+console.log(`${author} once said, "A person who never made a mistake never tried anything new."`)
